5d7a05dd26e646090320a6d4e2b47a5225637fa2
geekswg <geekswg@qq.com>
2023-01-30 14:01:16 +0800

    初始化python demo 爬虫实例

---
+++ hello.py
@@ -0,0 +1,11 @@
+""" 
+print("hello , python \n你好python")
+counter = 100 # 赋值整型变量
+miles = 1000.0 # 浮点型
+name = "John" # 字符串
+
+print (counter)
+print (miles)
+print (name)
+print ("counter+miles = " + str(counter+miles) ) """
+
